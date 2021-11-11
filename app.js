082dca4a41ebbfd619b86f80ec9dd2f6d37bf8a0
matthew640523 <52960782+matthew640523@users.noreply.github.com>
2021-11-11 10:40:13 -0500

    Uploading User App

---
+++ app.js
@@ -0,0 +1,60 @@
+//  GLOBAL VARIABLES
+let UsersContainer = document.querySelector('.UsersContainer')
+let UserBtn = document.querySelector('.UserBtn')
+
+
+// EVENT LISTENERS
+
+    window.addEventListener('DOMContentLoaded', Loadjson)
+    
+    UsersContainer.addEventListener('click', ShowPost)
+
+
+//JSON LOADING FUNCTION
+
+function Loadjson(){
+
+fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(response => response.json())
+    .then(data => {   
+
+        let html = ''     
+            data.map(PieceOfData => {
+            html+= `<div class="User"> <h1 class="UserId">User: ${PieceOfData.id} </h1>
+            <h3 class="UserName"><span class="UserNameTitle">USERNAME: &nbsp&nbsp</span>${PieceOfData.name}</h3><button class="UserBtn">Click to see post</button>
+            <p class="UserBody">${PieceOfData.body}</p> 
+        </div>`
+    
+
+    })
+    //PUTTING THE DATA ONTO THE SCREEN
+    UsersContainer.innerHTML = html
+})
+}
+function ShowPost(e){
+    if(e.target.classList.contains('UserBtn')){
+        //GETTING THE NAME OF THE ITEM THE USER SELECTED
+        let User = e.target.parentElement
+
+        let SelectedBodyClass = User.querySelector('.UserBody')
+
+        let SelectedUserBtn = User.querySelector('.UserBtn')
+
+        let SelectedUserName = User.querySelector('.UserName').textContent
+
+        let SelectedBody = User.querySelector('.UserBody').textContent
+
+        //TOGGLING THE BODY WHEN THE USER HITS THE BUTTON AND ALSO CHANGING THE STYLING
+        SelectedBodyClass.classList.toggle('Show')
+        SelectedUserBtn.textContent == 'Click to see post' ? e.target.textContent = 'Hide post': e.target.textContent = 'Click to see post';
+        
+        if(SelectedUserBtn.textContent !== 'Click to see post'){
+            SelectedUserBtn.style.backgroundColor = 'red'
+        }
+        else{
+            SelectedUserBtn.style.backgroundColor = 'rgb(0, 81, 255)'
+        }
+
+
+    }
+}
